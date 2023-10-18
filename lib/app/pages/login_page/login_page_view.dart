7f37c04974e1ea949a15537f319b8e66e704052d
uradev0123 <uradev0123@gmail.com>
2023-10-18 14:35:19 +0700

    feat : [SF] setup project (directory, route, textstyle)

---
+++ lib/app/pages/login_page/login_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPageView extends StatelessWidget {
+  const LoginPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
