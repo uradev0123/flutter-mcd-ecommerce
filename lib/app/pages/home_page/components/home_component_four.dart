79072610adea7be302e1132a38302734a36b79f7
SyahranFD <fadhilrafa1@gmail.com>
2023-10-29 21:09:40 +0700

    feat: [SF] make card into reusable widget and add images

--- lib/app/pages/home_page/components/home_component_four.dart
+++ lib/app/pages/home_page/components/home_component_four.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:web_pancasila_aceh/app/mock_data/controller/recommended.dart';
+import 'package:web_pancasila_aceh/app/pages/home_page/widgets/card_product_home_page.dart';
 import 'package:web_pancasila_aceh/common/helper/themes.dart';
 
 class HomeComponentFour extends StatelessWidget {
@@ -27,54 +28,7 @@ class HomeComponentFour extends StatelessWidget {
 
         SizedBox(height: height * 0.025),
 
-        Obx(() => Container(
-            height: height * 0.3,
-            margin: EdgeInsets.only(left: width * 0.05),
-            child: ListView.builder(
-              itemCount: recommendedController.recommended.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                var recommended = recommendedController.recommended[index];
-                var formattedPrice = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ').format(recommended.price);
-                formattedPrice = formattedPrice.replaceAll(",00", "");
-                return Container(
-                  width: width * 0.35,
-                  margin: EdgeInsets.only(right: width * 0.035),
-                  padding: EdgeInsets.all(width * 0.03),
-                  decoration: BoxDecoration(
-                      color: whiteColor,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          offset: Offset(0, 3),
-                          blurRadius: 6,)
-                      ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Transform.scale(
-                        scale: 1.25,
-                        child: Image.asset(recommended.image, fit: BoxFit.fitHeight),
-                      ),
-                      SizedBox(height: height * 0.02),
-                      Container(
-                        height: height * 0.045,
-                          child: Align(
-                            alignment: Alignment.bottomLeft,
-                              child: Text(recommended.name, style: ts10MediumBlack)
-                          ),
-                      ),
-                      SizedBox(height: height * 0.005),
-                      Text(formattedPrice, style: ts10MediumGrey),
-                    ],
-                  ),
-                );
-              },
-            )
-        )),
+        cardProductHomePage(context: context, controller: recommendedController),
       ],
     );
   }
---
+++ lib/app/pages/home_page/widgets/card_product_home_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import '../../../../common/helper/themes.dart';
+
+Widget cardProductHomePage ({required context, required controller}) {
+  final Size mediaQuery = MediaQuery.of(context).size;
+  final double width = mediaQuery.width;
+  final double height = mediaQuery.height;
+
+  return Obx(() => Container(
+      height: height * 0.295,
+      margin: EdgeInsets.only(left: width * 0.05),
+      child: ListView.builder(
+        itemCount: controller.recommended.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          var product = controller.recommended[index];
+          var formattedPrice = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ').format(product.price);
+          formattedPrice = formattedPrice.replaceAll(",00", "");
+          return Container(
+            width: width * 0.35,
+            margin: EdgeInsets.only(right: width * 0.035, bottom: height * 0.0075),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x3F000000),
+                  blurRadius: 4,
+                  offset: Offset(0, 3),
+                  spreadRadius: 0,
+                )
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(product.image, fit: BoxFit.fitHeight),
+                    SizedBox(height: height * 0.01),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.03),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: height * 0.045,
+                            child: Align(
+                                alignment: Alignment.bottomLeft,
+                                child: Text(product.name, style: ts10MediumBlack)
+                            ),
+                          ),
+                          SizedBox(height: height * 0.005),
+                          Text(formattedPrice, style: ts10MediumGrey),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: height * 0.035,
+                  width: double.infinity,
+                  margin: EdgeInsets.all(width * 0.015),
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(Icons.add, size: 20,),
+                ),
+              ],
+            ),
+          );
+        },
+      )
+  ));
+
+}
