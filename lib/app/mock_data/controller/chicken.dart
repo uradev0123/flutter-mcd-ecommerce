c1f49144fc9f3ed0729601ac67e909099902f85f
uradev0123 <uradev0123@gmail.com>
2023-10-30 16:52:08 +0700

    fix: [SF] change name class in controller breakfast, chicken, dessert, drink

--- lib/app/mock_data/controller/chicken.dart
+++ lib/app/mock_data/controller/chicken.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:web_pancasila_aceh/app/mock_data/model/product.dart';
 
-class BurgerController extends GetxController {
+class ChickenController extends GetxController {
   var chicken = <Product>[].obs;
 
   @override
