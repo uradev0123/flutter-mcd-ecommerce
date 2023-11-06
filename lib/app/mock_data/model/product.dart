77762f62ffc6d0c5997d887436f7c1bbef626fa7
SyahranFD <fadhilrafa1@gmail.com>
2023-11-06 16:33:01 +0700

    feat: [SF] add id mock data

--- lib/app/mock_data/model/product.dart
+++ lib/app/mock_data/model/product.dart
@@ -1,10 +1,12 @@
 class Product {
+  late String id;
   late String image;
   late String name;
   late double price;
   late double many;
 
-  Product(String image, String name, double price, {double many = 0}){
+  Product(String id, String image, String name, double price, {double many = 0}){
+    this.id = id;
     this.image = "assets/images/$image.png";
     this.name = name;
     this.price = price;
