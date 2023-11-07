fb2485a1f66675b92a45772673cf633c07237436
uradev0123 <uradev0123@gmail.com>
2023-11-07 07:50:33 +0700

    feat: [SF] add increment and decrement quantity

--- lib/app/pages/home_page/widgets/card_product_home_page.dart
+++ lib/app/pages/home_page/widgets/card_product_home_page.dart
@@ -20,15 +20,17 @@ Widget cardProductHomePage({required context, required controller}) {
           scrollDirection: Axis.horizontal,
           itemBuilder: (BuildContext context, int index) {
             var product = controller[index];
-            var formattedPrice = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ').format(product.price);
+            var formattedPrice =
+                NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ')
+                    .format(product.price);
             formattedPrice = formattedPrice.replaceAll(",00", "");
-            print(product.name + product.many.toString());
 
             return Obx(() {
-              bool isSelected = cartController.selectedProductIndex.value == index;
+              bool isSelected = cartController.isProductSelected(product.id);
               return Container(
                 width: width * 0.35,
-                margin: EdgeInsets.only(right: width * 0.035, bottom: height * 0.0075),
+                margin: EdgeInsets.only(
+                    right: width * 0.035, bottom: height * 0.0075),
                 decoration: BoxDecoration(
                   color: whiteColor,
                   borderRadius: BorderRadius.circular(10),
@@ -41,8 +43,8 @@ Widget cardProductHomePage({required context, required controller}) {
                     )
                   ],
                   border: Border.all(
-                    color: isSelected ? Colors.yellow : Colors.transparent,
-                    width: 3,
+                    color: isSelected ? primaryColor : Colors.transparent,
+                    width: 5,
                   ),
                 ),
                 child: Column(
@@ -53,7 +55,8 @@ Widget cardProductHomePage({required context, required controller}) {
                         Image.asset(product.image, fit: BoxFit.fitHeight),
                         SizedBox(height: height * 0.01),
                         Container(
-                          padding: EdgeInsets.symmetric(horizontal: width * 0.03),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: width * 0.03),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -73,23 +76,61 @@ Widget cardProductHomePage({required context, required controller}) {
                         ),
                       ],
                     ),
-                    Container(
-                      height: height * 0.035,
-                      width: double.infinity,
-                      margin: EdgeInsets.all(width * 0.015),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          cartController.selectedProductIndex.value = index;
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: primaryColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+                    isSelected
+                        ? Container(
+                            margin: EdgeInsets.all(width * 0.015),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    cartController.decrementProductQuantity(product);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    minimumSize: Size(width * 0.08, width * 0.08),
+                                    backgroundColor: primaryColor,
+                                  ),
+                                  child: Icon(Icons.remove, color: blackColor, size: 16),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.symmetric(horizontal: width * 0.001),
+                                  child: Text(product.quantity.toString(), style: ts14MediumBlack),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    cartController.incrementProductQuantity(product);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    shape: CircleBorder(),
+                                    minimumSize: Size(width * 0.08, width * 0.08),
+                                    backgroundColor: primaryColor,
+                                  ),
+                                  child: Icon(Icons.add, color: blackColor, size: 16),
+                                ),
+                              ],
+                            ),
+                          )
+                        : Container(
+                            height: height * 0.035,
+                            width: double.infinity,
+                            margin: EdgeInsets.all(width * 0.015),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                cartController.addToSelectedProducts(product.id);
+                                cartController.incrementProductQuantity(product);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: primaryColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child:
+                                  Icon(Icons.add, size: 20, color: blackColor),
+                            ),
                           ),
-                        ),
-                        child: Icon(Icons.add, size: 20, color: blackColor),
-                      ),
-                    ),
                   ],
                 ),
               );
