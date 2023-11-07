0615bb2a82a224668d5a280b440f971f6adf5d21
SyahranFD <fadhilrafa1@gmail.com>
2023-11-07 07:50:33 +0700

    feat: [SF] add increment and decrement quantity

--- lib/app/mock_data/model/product.dart
+++ lib/app/mock_data/model/product.dart
@@ -1,15 +1,17 @@
+import 'package:get/get.dart';
+
 class Product {
   late String id;
   late String image;
   late String name;
   late double price;
-  late double many;
+  RxInt quantity = 0.obs;
 
-  Product(String id, String image, String name, double price, {double many = 0}){
+  Product(String id, String image, String name, double price, {int quantity = 0}){
     this.id = id;
     this.image = "assets/images/$image.png";
     this.name = name;
     this.price = price;
-    this.many = many;
+    this.quantity.value = quantity;
   }
 }
