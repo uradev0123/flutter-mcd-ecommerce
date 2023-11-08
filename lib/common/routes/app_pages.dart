5553c19376f923f82e355574a814ec0aa800da57
SyahranFD <fadhilrafa1@gmail.com>
2023-11-08 23:55:30 +0700

    feat: [SF] add navbar

--- lib/common/routes/app_pages.dart
+++ lib/common/routes/app_pages.dart
@@ -22,7 +22,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.MENU_PAGE;
+  static const INITIAL = Routes.HOME_PAGE;
 
   static final routes = [
     GetPage(
