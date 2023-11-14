7091fa108bd9ec15930e96491cb4a593dc19fd7b
vaniafs <vaniafiryalodias@gmail.com>
2023-11-14 11:16:50 +0700

    feat:

---
+++ lib/app/pages/home_page/components/home_component_six.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mcd_ecommerce/common/helper/themes.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class HomeComponentSix extends StatelessWidget {
+  const HomeComponentSix({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size mediaQuery = MediaQuery.of(context).size;
+    final double width = mediaQuery.width;
+    final double height = mediaQuery.height;
+
+      return Column(
+    children: [
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('News', style: ts16SemiboldBlack),
+          ],
+        ),
+      ),
+      SizedBox(height: 10),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: StaggeredGrid.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 4,
+          children: [
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0), 
+                child: Image.asset('assets/images/imgA.png', fit: BoxFit.fitWidth),
+              ),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0), 
+                child: Image.asset('assets/images/imgB.png', fit: BoxFit.fitWidth),
+              ),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0), 
+                child: Image.asset('assets/images/imgC.png', fit: BoxFit.fitWidth),
+              ),
+            ),
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0), 
+                child: Image.asset('assets/images/imgD.png', fit: BoxFit.fitWidth),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+  }
+}
--- lib/common/routes/app_pages.dart
+++ lib/common/routes/app_pages.dart
@@ -21,7 +21,7 @@ class AppPages {
 
   AppPages._();
 
-  static const INITIAL = Routes.PROFILE_PAGE;
+  static const INITIAL = Routes.HOME_PAGE;
 
   static final routes = [
     GetPage(
